22df134ec6c0763e538ceca4779f28b9bc31ee40
fierylion <97876216+fierylion@users.noreply.github.com>
2022-09-11 19:46:42 +0300

    Create maze_generator.py

---
+++ maze_generator.py
@@ -0,0 +1,78 @@
+import numpy as np
+import random as rd
+# generates a square maze (size*size) with the Aldous-Broder algorithm --> Acknowledgment to Aldous-Broder
+class square_maze():
+    def __init__(self, size):
+        self.size = size
+    def ald(self, grid: np.ndarray, size: int) -> np.ndarray:
+        output_grid = np.empty([size * 3, size * 3],  dtype="object")
+        output_grid[:] = '#'
+        c = size * size  # number of cells to be visited
+        i = rd.randrange(size)
+        j = rd.randrange(size)
+        while np.count_nonzero(grid) < c:
+
+            # visit this cell
+            grid[i, j] = 1
+
+            w = i * 3 + 1
+            k = j * 3 + 1
+            output_grid[w, k] = ' '
+
+            can_go = [1, 1, 1, 1]
+
+            if i == 0:
+                can_go[0] = 0
+            if i == size - 1:
+                can_go[2] = 0
+            if j == 0:
+                can_go[3] = 0
+            if j == size - 1:
+                can_go[1] = 0
+
+            # it makes sense to choose neighbour among available directions
+            neighbour_idx = np.random.choice(np.nonzero(can_go)[0])  # n,e,s,w
+
+            if neighbour_idx == 0:
+                # has been visited?
+                if grid[i - 1, j] == 0:
+                    # goto n
+                    output_grid[w - 1, k] = ' '
+                    output_grid[w - 2, k] = ' '
+                i -= 1
+
+            if neighbour_idx == 1:
+                if grid[i, j + 1] == 0:
+                    # goto e
+                    output_grid[w, k + 1] = ' '
+                    output_grid[w, k + 2] = ' '
+                j += 1
+
+            if neighbour_idx == 2:
+                if grid[i + 1, j] == 0:
+                    # goto s
+                    output_grid[w + 1, k] = ' '
+                    output_grid[w + 2, k] = ' '
+                i += 1
+
+            if neighbour_idx == 3:
+                # goto w
+                if grid[i, j - 1] == 0:
+                    output_grid[w, k - 1] = ' '
+                    output_grid[w, k - 2] = ' '
+                j -= 1
+
+        return output_grid
+
+
+    def generate(self):
+        size = self.size
+
+        # np.random.seed(42)
+        grid = np.zeros(shape=(size, size))
+
+
+        console_grid = square_maze.ald(self, grid, size)
+        return console_grid
+
+
